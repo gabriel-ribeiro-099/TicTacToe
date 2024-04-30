f1a55799ae62e6874ea13690ecd2a5d20e506b40
gabriel-ribeiro-099 <gabriel.ribeiro.099@ufrn.edu.br>
2024-04-30 14:06:26 -0300

    Tentando faer GitHub PAges funcionar

--- Index.js
+++ Index.js
@@ -3,7 +3,7 @@ function iniciarJogo() {
     var tamanhoSelecionado = document.getElementById("tamanhoTabuleiro").value;
     
     switch (tamanhoSelecionado) {
-        case "3x3": window.location.href = "../html/3x3.html"; break;
+        case "3x3": window.location.href = "html/3x3.html"; break;
         case "4x4": window.location.href = "../html/4x4.html"; break;
         case "5x5": window.location.href = "../html/5x5.html"; break;
         case "6x6": window.location.href = "../html/6x6.html"; break;
